ff93ace71d20d396bd3f17441e93d49a143122e4
Raphael Raja <jrrphonix@gmail.com>
2020-02-07 02:04:06 +0530

    First commit

--- src/speedConverter.java
+++ src/speedConverter.java
@@ -1,5 +1,16 @@
 public class speedConverter {
     public static void main(String[] args) {
+        toMilesPerHour(1);
+    }
 
+    public static double toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            return -1;
+        }
+        else{
+            double milesPerHour =  kilometersPerHour / 1.609344;
+            System.out.println(milesPerHour);
+            return milesPerHour;
+        }
     }
 }
