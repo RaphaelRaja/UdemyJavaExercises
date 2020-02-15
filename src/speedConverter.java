e3cd4e423ae915bd2386b1f30eab9e5892b5a390
Raphael Raja <jrrphonix@gmail.com>
2020-02-15 17:25:11 +0530

    Completed SpeedConverter Challenge

--- src/speedConverter.java
+++ src/speedConverter.java
@@ -1,16 +1,23 @@
 public class speedConverter {
-    public static void main(String[] args) {
-        toMilesPerHour(1);
-    }
 
-    public static double toMilesPerHour(double kilometersPerHour){
+    public static long toMilesPerHour(double kilometersPerHour){
         if(kilometersPerHour < 0){
             return -1;
         }
-        else{
-            double milesPerHour =  kilometersPerHour / 1.609344;
-            System.out.println(milesPerHour);
-            return milesPerHour;
+
+        long milesPerHour  = Math.round(kilometersPerHour/1.609);
+        return milesPerHour;
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            System.out.println("Invalid Value");
         }
+        else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    " km/h = "+ milesPerHour+" mi/h");
+        }
+
     }
 }
