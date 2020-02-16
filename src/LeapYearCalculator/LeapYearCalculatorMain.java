d422bb7db24520db82f9b06659883a560e928899
Raphael Raja <jrrphonix@gmail.com>
2020-02-16 14:11:12 +0530

    Completed LeapYearCalculatorSolution

---
+++ src/LeapYearCalculator/LeapYearCalculatorMain.java
@@ -0,0 +1,10 @@
+package LeapYearCalculator;
+
+public class LeapYearCalculatorMain {
+    public static void main(String[] args) {
+        System.out.println(LeapYearCalculatorSolution.isLeapYear(-1600));
+        System.out.println(LeapYearCalculatorSolution.isLeapYear(1600));
+        System.out.println(LeapYearCalculatorSolution.isLeapYear(2017));
+        System.out.println(LeapYearCalculatorSolution.isLeapYear(2000));
+    }
+}
