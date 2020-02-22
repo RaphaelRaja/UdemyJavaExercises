fd4ffedb8b983ee62b9533995be522b07537da55
Raphael Raja <jrrphonix@gmail.com>
2020-02-23 01:54:30 +0530

    Completed AreaCalculatorSolution

---
+++ src/AreaCalculator/AreaCalculatorMain.java
@@ -0,0 +1,10 @@
+package AreaCalculator;
+
+public class AreaCalculatorMain {
+    public static void main(String[] args) {
+        System.out.println(AreaCalculatorSolution.area(5.0));
+        System.out.println(AreaCalculatorSolution.area(-1));
+        System.out.println(AreaCalculatorSolution.area(5.0, 4.0));
+        System.out.println(AreaCalculatorSolution.area(-1.0, 4.0));
+    }
+}
