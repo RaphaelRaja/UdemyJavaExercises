9c461468fd9864b1579dd9fdf63546e64e4c6298
Raphael Raja <jrrphonix@gmail.com>
2020-02-23 22:59:03 +0530

    Completed NumberInWordSolution

---
+++ src/NumberInWord/NumberInWordMain.java
@@ -0,0 +1,10 @@
+package NumberInWord;
+
+public class NumberInWordMain {
+    public static void main(String[] args) {
+        NumberInWordSolution.printNumberInWord(0);
+        NumberInWordSolution.printNumberInWord(5);
+        NumberInWordSolution.printNumberInWord(-1);
+
+    }
+}
