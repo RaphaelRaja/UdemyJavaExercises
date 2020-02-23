21fa52893e5df74dcd55d94472ad780e9046e103
Raphael Raja <jrrphonix@gmail.com>
2020-02-23 22:34:29 +0530

    Completed PlayingCatSolution

---
+++ src/PlayingCat/PlayingCatMain.java
@@ -0,0 +1,9 @@
+package PlayingCat;
+
+public class PlayingCatMain {
+    public static void main(String[] args) {
+        System.out.println(PlayingCatSolution.isCatPlaying(true, 25));
+        System.out.println(PlayingCatSolution.isCatPlaying(false, 36));
+        System.out.println(PlayingCatSolution.isCatPlaying(false, 35));
+    }
+}
---
+++ src/PlayingCat/PlayingCatSolution.java
@@ -0,0 +1,13 @@
+package PlayingCat;
+
+public class PlayingCatSolution {
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if((summer == false) && ((temperature >= 25) && (temperature <= 35))){
+            return true;
+        }
+        else if((summer == true) && ((temperature >= 25) && (temperature <= 45))){
+            return true;
+        }
+        return false;
+    }
+}
