3d1cfcc4c74726809307880d12281e9484c0403a
Raphael Raja <jrrphonix@gmail.com>
2020-02-23 20:26:03 +0530

    Completed AreaCalculatorSolution

---
+++ src/MinutesToYearsDaysCalculator/MinutesToYearsDaysCalculatorMain.java
@@ -0,0 +1,9 @@
+package MinutesToYearsDaysCalculator;
+
+public class MinutesToYearsDaysCalculatorMain {
+    public static void main(String[] args) {
+        MinutesToYearsDaysCalculatorSolution.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculatorSolution.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculatorSolution.printYearsAndDays(561600);
+    }
+}
