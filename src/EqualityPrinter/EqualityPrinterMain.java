5974eef15df18b16e50cd33463d184dfe39e73f6
Raphael Raja <jrrphonix@gmail.com>
2020-02-23 21:00:46 +0530

    Completed EqualityPrinterSolution

---
+++ src/EqualityPrinter/EqualityPrinterMain.java
@@ -0,0 +1,10 @@
+package EqualityPrinter;
+
+public class EqualityPrinterMain {
+    public static void main(String[] args) {
+        EqualityPrinterSolution.printEqual(1, 1, 1);
+        EqualityPrinterSolution.printEqual(1, 1, 2);
+        EqualityPrinterSolution.printEqual(-1, -1, -1);
+        EqualityPrinterSolution.printEqual(1, 2, 3);
+    }
+}
